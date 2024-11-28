3ea58ca4045a028720dfbd06fd8c53650031f24f
Macapps13 <ben.alvaro@icloud.com>
2024-11-28 12:55:52 +1100

    Added Aliens + Movement, w/o lasers

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame, sys
 from player import Player
 import obstacle
+from alien import Alien
+from random import choice
 
 class Game:
 	def __init__(self):
@@ -15,6 +17,11 @@ class Game:
 		self.obstacle_amount = 4
 		self.obstacle_x_positions = [num*(screen_width/self.obstacle_amount) for num in range(self.obstacle_amount)]
 		self.create_multiple_obstacles(*self.obstacle_x_positions, x_start = screen_width/15, y_start = 480)
+
+		#Alien setup
+		self.aliens = pygame.sprite.Group()
+		self.alien_setup(rows = 6, cols = 8)
+		self.alien_direction = 1
 	
 	def create_obstacle(self, x_start, y_start, offset_x):
 		for row_index, row in enumerate(self.shape):
@@ -29,14 +36,46 @@ class Game:
 		for offset_x in offset:
 			self.create_obstacle(x_start, y_start, offset_x)
 
+	def alien_setup(self, rows, cols, x_distance = 60, y_distance = 48, x_offset = 70, y_offset = 100):
+		for row_index, row in enumerate(range(rows)):
+			for col_index, col in enumerate(range(cols)):
+				x = col_index * x_distance + x_offset
+				y = row_index * y_distance + y_offset
+
+				if row_index == 0: alien_sprite = Alien('yellow', x, y)
+				elif 1<= row_index <= 2: alien_sprite = Alien('green', x, y)
+				else: alien_sprite = Alien('red', x, y)
+				self.aliens.add(alien_sprite)
+
+	def alien_position_checker(self):
+		all_aliens = self.aliens.sprites()
+		for alien in all_aliens:
+			if alien.rect.right >= screen_width:
+				self.alien_direction = -1
+				self.alien_move_down(2)
+			elif alien.rect.left <= 0:
+				self.alien_direction = 1
+				self.alien_move_down(2)
+
+	def alien_move_down(self, distance):
+		if self.aliens:
+			for alien in self.aliens.sprites():
+				alien.rect.y += distance
+
+	def alien_shoow(self):
+		if self.aliens.sprites():
+			random_alien = choice(self.aliens.sprites())
 
 	def run(self):
 		self.player.update()
+		self.aliens.update(self.alien_direction)
+		self.alien_position_checker()
 
 		self.player.sprite.lasers.draw(screen)
 		self.player.draw(screen)
 
 		self.blocks.draw(screen)
+		self.aliens.draw(screen)
 		#update and draw all sprite groups
 
 
